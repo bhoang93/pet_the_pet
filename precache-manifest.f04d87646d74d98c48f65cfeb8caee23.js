614bde159c355824638f4decfe8c11389120ea48
unknown <brian@digital-labs.co.uk>
2020-07-02 18:44:40 +0100

    Updates

---
+++ precache-manifest.f04d87646d74d98c48f65cfeb8caee23.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "074edce3ee15c0e717901bc09043a703",
+    "url": "/pet_the_pet/index.html"
+  },
+  {
+    "revision": "edbc2733c6c38849d619",
+    "url": "/pet_the_pet/static/css/main.2be81c70.chunk.css"
+  },
+  {
+    "revision": "c8cfda9541638863e6a5",
+    "url": "/pet_the_pet/static/js/2.0c4872b0.chunk.js"
+  },
+  {
+    "revision": "bf32bd79d072257647d60a812b360922",
+    "url": "/pet_the_pet/static/js/2.0c4872b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "edbc2733c6c38849d619",
+    "url": "/pet_the_pet/static/js/main.7ac08695.chunk.js"
+  },
+  {
+    "revision": "1285a0cc7093d7c123e2",
+    "url": "/pet_the_pet/static/js/runtime-main.309ceac6.js"
+  },
+  {
+    "revision": "65043f592abbbb58907b1ad54056fca7",
+    "url": "/pet_the_pet/static/media/logo.65043f59.png"
+  }
+]);
